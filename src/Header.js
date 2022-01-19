ec4b32ee78a3aeac3bda147bbd7e1dbbba5eb649
Richy Jay <tabufx50@hotmail.com>
2022-01-19 10:12:56 -0700

    added middle header

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./Header.css";
 import MenuIcon from '@material-ui/icons/Menu';
 import { IconButton } from '@material-ui/core';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function Header() {
     return ( 
@@ -13,7 +15,9 @@ function Header() {
                 <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" /> 
             </div>
             <div className="header__middle">
-
+                <SearchIcon />
+                <input placeholder='Search mail' type="text" />
+                <ArrowDropDownIcon className="header__inputCaret" />
             </div>
             <div className="header__right">
 
