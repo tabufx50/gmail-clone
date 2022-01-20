13b7877369114184d0fd721ae1ec13cb7caa354b
Richy Jay <tabufx50@hotmail.com>
2022-01-20 15:52:26 -0700

    added inside top email section

--- src/EmailList.js
+++ src/EmailList.js
@@ -8,6 +8,10 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
 import SettingsIcon from '@mui/icons-material/Settings';
+import Section from './Section';
+import InboxIcon from '@mui/icons-material/Inbox';
+import PeopleIcon from '@mui/icons-material/People';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 function EmailList() {
   return <div className='emailList'>
@@ -37,8 +41,13 @@ function EmailList() {
             <IconButton>
                 <SettingsIcon />
             </IconButton>
-
           </div>
+      </div>
+
+      <div className='emailList__sections'>
+          <Section Icon={InboxIcon} title='primary' color='red' selected />
+          <Section Icon={PeopleIcon} title='Social' color='#1A73E8' />
+          <Section Icon={LocalOfferIcon} title='Promotions' color='green' />
       </div>
   </div>;
 }
