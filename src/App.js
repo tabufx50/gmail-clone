3d34691e342d6b5ae9e2d528bba6a03b15d73167
Richy Jay <tabufx50@hotmail.com>
2022-01-20 08:49:15 -0700

    added react router dom v 6

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Mail from './Mail';
+import EmailList from './EmailList';
 
 function App() {
   return (
+    <Router>
     <div className="app">
     <Header />
+
+    <div className='app__body'>
       <Sidebar />
+
+      <Routes>
+        <Route path="/mail" component={<Mail/>}
+         />
+          <Route path="/" component={<EmailList />}
+          />
+      </Routes>
     </div>
+    </div>
+    </Router>
   );
 }
 
